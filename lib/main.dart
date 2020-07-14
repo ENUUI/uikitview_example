eb10de3967b1396e91f64a525c871c3d861aedd2
chenfuhui <chenfuhui@xxbmm.com>
2020-07-14 11:35:42 +0800

    example of flutter #61340

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:uikitview_example/uikitview_example.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(home: Home());
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(title: Text('EXAMPLE')),
+        body: ListView.separated(
+          itemBuilder: (context, index) => Container(
+            margin: EdgeInsets.symmetric(horizontal: 20.0),
+            height: 300.0,
+            child: IOSView(),
+          ),
+          separatorBuilder: (context, index) => SizedBox(height: 20.0),
+          itemCount: 20,
+        ),
+      );
+}
